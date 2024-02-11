4a2012551a20efcbd947a9681994938c62e4fbd2
limlingan <limlinganangela@gmail.com>
2024-02-12 00:06:32 +0800

    modifications for HomePage -- setup Search Page

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./Home.css";
 import { Link } from "react-router-dom"; 
+import AppsIcon from '@mui/icons-material/Apps';
+import { Avatar } from '@mui/material';
+import Search from './Search';
 
 function Home() {
   return (
     <div className="home">
-        <h1>This HOMEPAGE</h1>
       <div className="home__header">
         <div className="home__headerLeft">
           <Link to='/about'>About</Link>
@@ -14,7 +16,15 @@ function Home() {
         <div className="home__headerRight">
           <Link to='/gmail'>Gmail</Link>
           <Link to='/images'>Images</Link>
-          
+          <AppsIcon />
+          <Avatar />
+        </div>
+      </div>
+
+      <div className='home__body'>
+        <img src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'></img>
+        <div className='home__inputContainer'>
+          <Search />
         </div>
       </div>
     </div>
