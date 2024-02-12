73cd2c08796df9fc2229c55d2c34e1ddd90c7e96
limlingan <limlinganangela@gmail.com>
2024-02-12 13:26:54 +0800

    Google Search Bar and search function

--- src/Search.js
+++ src/Search.js
@@ -1,19 +1,33 @@
-import React from "react";
-import './Search.css';
-import SearchIcon from '@mui/icons-material/Search';
-import MicIcon from '@mui/icons-material/Mic';
+import React, { useState } from "react";
+import "./Search.css";
+import SearchIcon from "@mui/icons-material/Search";
+import MicIcon from "@mui/icons-material/Mic";
+import Button from "@mui/material/Button";
 
 function Search() {
+  const [input, setInput] = useState("");
+
+  const search = (e) => {
+    e.preventDefault();
+    console.log('successful >>', input);
+  };
+
   return (
-    <div className="search">
+    <form className="search">
       <div className="search__input">
         <SearchIcon className="search__inputIcon" />
-        <input />
+        <input value={input} onChange={e => setInput(e.target.value)} />
         <MicIcon />
       </div>
 
-    </div>
-  )
+      <div className="search__buttons">
+        <Button type='submit' onClick={search} variant="outlined">
+          Google Search
+        </Button>
+        <Button variant="outlined">I'm Feeling Lucky</Button>
+      </div>
+    </form>
+  );
 }
 
-export default Search
+export default Search;
